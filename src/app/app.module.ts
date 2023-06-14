cbd583d11080b2b38e209eaa6aa3e2d634849e67
hemanth04/09 <hemanthbalakrishnan2002@gmail.com>
2023-06-14 14:24:15 +0530

    recent commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+import { ButtonModule } from 'primeng/button';
+import { SidebarModule } from 'primeng/sidebar';
+
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CardModule } from 'primeng/card';
+import { TimelineModule } from 'primeng/timeline';
+import {MenuModule} from 'primeng/menu';
+import { CarouselComponent } from './carousel/carousel.component';
+import {DataViewModule} from 'primeng/dataview';
+import { DataviewComponent } from './dataview/dataview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    CarouselComponent,
+    DataviewComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+     BrowserAnimationsModule,
+    ButtonModule,
+    SidebarModule,
+    CardModule,
+    TimelineModule,
+    MenuModule,
+    DataViewModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'; 
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+  itemImageUrl ='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVF69-ec7euYv1FdP7kohXhw1ijow7fr2TojQPhH7kRQ&usqp=CAU&ec=48600113'
+}
